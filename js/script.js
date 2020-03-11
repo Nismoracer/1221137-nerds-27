fd95587addd036ce197485c84a35416803d38167
Nismoracer <ilya.grunov@gmail.com>
2020-03-12 00:20:44 +0300

    Добавлен слайдер на JS
    Изменение состояния слайдера не сохраняется. Возвращается в дефолтное состояние.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-var link = document.querySelector(".user-navigation");
-console.log(link);
+
+var art1 = document.querySelector(".article1");
+var art2 = document.querySelector(".article2");
+var art3 = document.querySelector(".article3");
+
+function currentSlide(n)    {
+    if (n == 1)
+    {
+        art1.style.display = "block";
+        art2.style.display = "none";
+        art3.style.display = "none";
+    }
+    if (n == 2)
+    {
+        art1.style.display = "none";
+        art2.style.display = "block";
+        art3.style.display = "none";
+    }
+    if (n == 3)
+    {
+        art1.style.display = "none";
+        art2.style.display = "none";
+        art3.style.display = "block";
+    }
+    
+}
