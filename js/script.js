e1e87a4b25d0a1626e0c7b051ebe9e2cedc28851
Nismoracer <ilya.grunov@gmail.com>
2020-03-13 09:10:23 +0300

    Сайт готов
    Проверено переполнение раздела с партнерами. Реализован слайдер и попан на JS

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
+let art1 = document.querySelector(".article1");
+let art2 = document.querySelector(".article2");
+let art3 = document.querySelector(".article3");
 
-var art1 = document.querySelector(".article1");
-var art2 = document.querySelector(".article2");
-var art3 = document.querySelector(".article3");
+let writeus = document.querySelector(".map-button");
+let popup = document.querySelector(".modal");
+let closeBut = document.querySelector(".modal-close");
+
+writeus.addEventListener("click", function(evt)     {
+    evt.preventDefault();
+    popup.classList.add("element-show");
+    popup.classList.add("modal-animation");
+});
+
+closeBut.addEventListener("click", function()     {
+    popup.classList.remove("element-show");
+    popup.classList.remove("modal-animation");
+    popup.classList.add("element-hide");
+});
 
 function currentSlide(n)    {
-    if (n == 1)
+    if (n === 1)
     {
         art1.style.display = "block";
         art2.style.display = "none";
         art3.style.display = "none";
     }
-    if (n == 2)
+    if (n === 2)
     {
         art1.style.display = "none";
         art2.style.display = "block";
         art3.style.display = "none";
     }
-    if (n == 3)
+    if (n === 3)
     {
         art1.style.display = "none";
         art2.style.display = "none";
         art3.style.display = "block";
     }
-    
 }
